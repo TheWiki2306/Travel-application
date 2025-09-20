c9d4656c393cfad3ca27fa0e225f4cc501c6581c
olusegun <olusegundavids1@gmail.com>
2025-09-20 22:40:59 +0100

    primary dashboard layout

--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Header } from "components";
 
 const AllUsers = () => {
-  return <div>AllUsers</div>;
+  return (
+    <main className="dashboard wrapper">
+      <Header title="Trips page" description="Check out current users" />
+      All users page content
+    </main>
+  );
 };
 
 export default AllUsers;
